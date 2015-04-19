3f2a1c46932137fee7caba589bec85be29a083e1
esorokowsky <esorokowsky@119e2a47-ad63-46af-b536-baa464241ec5>
2015-04-19 01:14:18 +0000

    added missing file

---
+++ Robotic Systems/Final Project/Unity/Chess/Assets/Scripts/GamePiece.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamePiece 
+{
+    public GameObject Object;
+    public int MarkerNum;
+
+    public Vector2 CurLoc;
+    public Vector2 PrevLoc;
+
+    public int Player;
+
+    public bool CanReverse = false;
+
+    public GamePiece()
+    {
+    }
+
+    public GamePiece(GameObject gameObject, Vector2 startLoc, Material material, int id, int player)
+	{
+        Object = gameObject;
+		CurLoc = startLoc;
+        MarkerNum = id;
+        Player = player;
+
+		if(material != null)
+            Object.GetComponent<Renderer>().material = material;
+
+		MovePiece(startLoc);
+	}
+
+	public void MovePiece(Vector2 newLoc)
+	{
+		PrevLoc = CurLoc;
+		CurLoc = newLoc;
+
+        Object.transform.position = Utilities.IndexToWorld(newLoc);
+
+        //For checkers you can move backwards after getting to the other side
+        if(Player == 1)
+        {
+            if (CurLoc.x == 7)
+                CanReverse = true;
+        }
+        else if(Player == 2)
+        {
+            if (CurLoc.x == 0)
+                CanReverse = true;
+        }
+
+		//Debug.Log(Model.name + " " + newLoc.ToString() + " " + Model.transform.position.ToString());
+	}
+}
